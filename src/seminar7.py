b23065878323d41ab1e9c2d1c64f3cb245890db6
dyyyaaayay <ksdlny@gmail.com>
2024-01-09 01:16:46 +0300

    sem7

--- src/seminar7.py
+++ src/seminar7.py
@@ -1,4 +1,4 @@
- """
+"""
 Seminar 7. SMS spam Classification with Recurrent Nets.
 https://www.kaggle.com/code/darthmanav/rnn-for-text-classification
 https://www.tensorflow.org/guide/keras/working_with_rnns
@@ -12,17 +12,15 @@ import boto3
 import dotenv
 import pandas as pd
 import tensorflow as tf
-from keras.layers import SimpleRNN, Activation, Dense, Dropout, Input, Embedding, LSTM
 
-MAX_WORDS = 1000
-MAX_SEQ_LEN = 150
+MAX_WORDS = 3456
+MAX_SEQ_LEN = 78
 DATA_URL_TRAIN = 'https://storage.yandexcloud.net/fa-bucket/spam.csv'
 DATA_URL_TEST = 'https://storage.yandexcloud.net/fa-bucket/spam_test.csv'
-PATH_TO_TRAIN_DATA = '../data/raw/spam.csv'
-PATH_TO_TEST_DATA = '../data/raw/spam_test.csv'
+PATH_TO_TRAIN_DATA = 'data/raw/spam.csv'
+PATH_TO_TEST_DATA = 'data/raw/spam_test.csv'
 PATH_TO_MODEL = 'models/model_7'
 BUCKET_NAME = 'neuralnets2023'
-# todo fix your git user name
 YOUR_GIT_USER = 'dyyyaaayay'
 
 
@@ -43,18 +41,20 @@ def make_model():
     :return:
     """
     inputs = tf.keras.layers.Input(name='inputs', shape=[MAX_SEQ_LEN])
-    x = tf.keras.layers.Embedding(MAX_WORDS, output_dim=50, input_length=MAX_SEQ_LEN)(inputs)
-    x = tf.keras.layers.SimpleRNN(units=4)(x)
-    x = Dense(256, name='FC1', activation='relu')(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
+
+    x = tf.keras.layers.Embedding(MAX_WORDS, output_dim=8, input_length=MAX_SEQ_LEN)(inputs)
+    x = tf.keras.layers.LSTM(units=16, return_sequences=True)(x)
+    x = tf.keras.layers.Dropout(0.4)(x)
+    x = tf.keras.layers.LSTM(units=8)(x)
+    x = tf.keras.layers.Dropout(0.4)(x)
     x = tf.keras.layers.Dense(1, name='out_layer')(x)
     x = tf.keras.layers.Activation('sigmoid')(x)
+
     recurrent_model = tf.keras.Model(inputs=inputs, outputs=x)
     return recurrent_model
 
 
-def load_data(csv_path='../data/raw/spam.csv') -> tuple:
+def load_data(csv_path='data/raw/spam.csv') -> tuple:
     df = pd.read_csv(csv_path)
     X = df.x.astype('str')
     Y = df.y.astype('int')
@@ -63,6 +63,7 @@ def load_data(csv_path='../data/raw/spam.csv') -> tuple:
 
 def train():
     X_train, Y_train = load_data()
+
     tok = tf.keras.preprocessing.text.Tokenizer(num_words=MAX_WORDS)
     tok.fit_on_texts(X_train)
     sequences = tok.texts_to_sequences(X_train)
@@ -70,21 +71,26 @@ def train():
 
     model = make_model()
     model.summary()
-    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy', tf.keras.metrics.Precision()])
-    model.fit(sequences_matrix, Y_train, batch_size=256, epochs=15, validation_split=0.2)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy', tf.keras.metrics.Precision()])
+    model.fit(sequences_matrix, Y_train, batch_size=128, epochs=10, validation_split=0.2)
     model.save('models/model_7')
+
     return tok
 
 
-def validate(model_path='models/model_7', tokenizer=None) -> tuple:
+def validate(model_path='models/model_7') -> tuple:
     """
     Validate model on test subset
     todo fit tokenizer on train texts,
     todo achieve >0.95 both accuracy and precision
     """
     model = tf.keras.models.load_model(model_path)
-    X_test, Y_test = load_data('../data/raw/spam_test.csv')
-    tok = tokenizer
+    X_test, Y_test = load_data('data/raw/spam_test.csv')
+
+    tok = tf.keras.preprocessing.text.Tokenizer(num_words=MAX_WORDS)
+    tok.fit_on_texts(X_test)
     test_sequences = tok.texts_to_sequences(X_test)
     test_sequences_matrix = tf.keras.preprocessing.sequence.pad_sequences(test_sequences, maxlen=MAX_SEQ_LEN)
 
@@ -97,13 +103,13 @@ def validate(model_path='models/model_7', tokenizer=None) -> tuple:
 def upload():
     """Pipeline: Upload model to S3 storage"""
     print('Upload model...')
-    zip_model_path = PATH_TO_MODEL+'.zip'
+    zip_model_path = PATH_TO_MODEL + '.zip'
     shutil.make_archive(base_name=PATH_TO_MODEL,
                         format='zip',
                         root_dir=PATH_TO_MODEL)
-    config = dotenv.dotenv_values('.env')
-    ACCESS_KEY = config['ACCESS_KEY']
-    SECRET_KEY = config['SECRET_KEY']
+    config = dotenv.dotenv_values('..env')
+    ACCESS_KEY = 'YCAJEKTT2vSJlrWgSP8q4jBtT'
+    SECRET_KEY = 'YCPsIQfgB3bneV3Koxab0vi_rDXM2WQcs-FigSBm'
 
     client = boto3.client(
         's3',
@@ -111,7 +117,7 @@ def upload():
         aws_access_key_id=ACCESS_KEY,
         aws_secret_access_key=SECRET_KEY
     )
-    client.upload_file(zip_model_path, BUCKET_NAME, f'{YOUR_GIT_USER}/model_7_s3.zip')
+    client.upload_file(zip_model_path, BUCKET_NAME, f'{YOUR_GIT_USER}/model_7.zip')
     print('Upload succeed.')
 
 
@@ -129,7 +135,6 @@ if __name__ == '__main__':
     if args.train:
         train()
     if args.validate:
-        token = train()
-        validate(tokenizer = token)
+        validate()
     if args.upload:
         upload()
